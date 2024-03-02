2d93bff62a6756353c41fd586faef7d5c90382dc
Tai Huynh <69029687+1BarCode@users.noreply.github.com>
2024-03-02 03:52:11 -0800

    refactored code to prevent deadlock during transaction

--- db/sqlc/transfer.go
+++ db/sqlc/transfer.go
@@ -30,6 +30,7 @@ func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (Tr
 	// TODO: rewrite execTx to be a generic function to return any type of result so we can get rid of the closure
 	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
+
 		// TODO: result.Transfer is a closure variable
 		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams(arg))
 		if err != nil {
@@ -52,43 +53,31 @@ func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (Tr
 			return err
 		}
 
-		// TODO: update accounts' balance
-		
+		// update accounts' balance
 		if arg.FromAccountID.String() < arg.ToAccountID.String() {
-			result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				ID:     arg.FromAccountID,
-				Amount: -arg.Amount,
-			})
-			if err != nil {
-				return err
-			}
-
-			result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				ID:     	arg.ToAccountID,
-				Amount: 	arg.Amount,
-			})
-			if err != nil {
-				return err
-			}
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
 		} else {
-			result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				ID:     	arg.ToAccountID,
-				Amount: 	arg.Amount,
-			})
-			if err != nil {
-				return err
-			}
-
-			result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				ID:     arg.FromAccountID,
-				Amount: -arg.Amount,
-			})
-			if err != nil {
-				return err
-			}
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
 		}
 
-		return nil
+		return err
 	})
+
 	return result, err
+}
+
+func addMoney(ctx context.Context, q *Queries, accountID1 uuid.UUID, amount1 int64, accountID2 uuid.UUID, amount2 int64) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     	accountID2,
+		Amount: 	amount2,
+	})
+	return
 }
